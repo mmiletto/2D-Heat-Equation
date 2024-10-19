05eb9430c0e3bd3e35d21bdf2de46694a660bf2a
Marcelo Cogo Miletto <marcelocm97@gmail.com>
2024-10-19 08:44:08 -0300

    move scripts to another directory.

--- scripts/python/plot_mesh.py
+++ scripts/python/plot_mesh.py
@@ -16,7 +16,14 @@ triangulation = mtri.Triangulation(data["x"], data["y"], data["triangles"])
 
 # Plot the triangular mesh
 plt.figure()
-plt.triplot(triangulation, 'go-', lw=1.5, alpha=0.3)  # green dots and lines
+plt.triplot(triangulation, 'go-', lw=1.5, alpha=0.3)
+
+# Annotate the vertices index
+for i, p in enumerate(zip(data["x"], data["y"])):
+    x = p[0]
+    y = p[1]
+    plt.text(x, y, str(i), fontsize=12, ha='right', color='blue')  # Add the index next to the point
+
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Triangular Mesh')
